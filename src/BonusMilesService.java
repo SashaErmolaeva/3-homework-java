5c2af849b8ba67b8407708333f26c19b81f63b29
Sasha Ermolaeva <sakharovaalex@gmail.com>
2022-11-03 20:53:17 +0300

    UpDate

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int milePrice = 20;
+        int miles;
+        miles = cost / milePrice;
+        return miles;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
-
     public static void main(String[] args) {
-        int ticketPrice = 20_000; // стоимость одного билета
-        int milePrice = 20; // стоимость одной мили
-        int numberTicket = 1; // количество билетов
-        int bonusMile = numberTicket * ticketPrice / milePrice;
-
-        System.out.println("Ваше количество бонусных миль за покупку билета:");
-        System.out.println(bonusMile);
-        
+        BonusMilesService service = new BonusMilesService();
+        int price = 25_234;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
+    //public static void main(String[] args) {
+        //int ticketPrice = 20_000; // стоимость одного билета
+        //int milePrice = 20; // стоимость одной мили
+       // int numberTicket = 1; // количество билетов
+        //int bonusMile = numberTicket * ticketPrice / milePrice;
+
+        //System.out.println("Ваше количество бонусных миль за покупку билета:");
+        //System.out.println(bonusMile);
+
 }
